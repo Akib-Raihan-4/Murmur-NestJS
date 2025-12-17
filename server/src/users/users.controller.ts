d259146df033717d0d6f369a21baaecf6d73866c
Akib Raihan <raihanakib062@gmail.com>
2025-12-17 18:01:32 +0600

    feat(MM-10): get all users and also isfollowing status (#10)

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import {
   Controller,
+  DefaultValuePipe,
   Get,
   Param,
   ParseIntPipe,
+  Query,
   UseGuards,
 } from "@nestjs/common";
 import { CurrentUser } from "src/auth/decorators/current-user.decorator";
@@ -23,17 +25,39 @@ export class UsersController {
   async getMyProfile(
     @CurrentUser() user: User
   ): Promise<IApiResponse<IProfileData>> {
-    const profile = await this.usersService.getUserProfile(user.id);
+    const profile = await this.usersService.getUserProfile(user.id, user.id);
 
     return successResponse(profile, "Profile fetched successfully");
   }
 
   @Get(":id")
   async getUserProfile(
+    @CurrentUser() currentUser: User,
     @Param("id", ParseIntPipe) userId: number
   ): Promise<IApiResponse<IProfileData>> {
-    const profile = await this.usersService.getUserProfile(userId);
+    const profile = await this.usersService.getUserProfile(
+      userId,
+      currentUser.id
+    );
 
     return successResponse(profile, "User profile fetched successfully");
+  }
+
+  @Get()
+  async getAllUsers(
+    @CurrentUser() currentUser: User,
+    @Query("page", new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query("limit", new DefaultValuePipe(10), ParseIntPipe) limit: number = 10
+  ): Promise<IApiResponse<IProfileData[]>> {
+    const result = await this.usersService.getAllUsers(currentUser.id, {
+      page,
+      limit,
+    });
+
+    return successResponse(
+      result.data,
+      "Users fetched successfully",
+      result.pagination
+    );
   }
 }
--- server/src/users/users.intefaces.ts
+++ server/src/users/users.intefaces.ts
@@ -4,4 +4,5 @@ export interface IProfileData {
   name: string;
   followersCount: number;
   followingCount: number;
+  isFollowing: boolean;
 }
